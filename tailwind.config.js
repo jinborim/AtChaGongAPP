d62acc88e1681b8eb54de02c9f39352fb8a49db3
dahlia0916 <dahlia3511@hanyang.ac.kr>
2026-08-10 20:24:41 +0900

    feat: NativeWind설정 및 디자인 시스템 추가

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+
+  presets: [require("nativewind/preset")],
+
+  theme: {
+    extend: {
+      colors: {
+        white: "#E8F3FC",
+        primary: "#18335E",
+        secondary: "#73C0FF",
+        gray: {
+          100: "#C7CED3",
+          300: "#A2AAB0",
+        },
+      },
+      fontFamily: {
+        maru: ["Mulmaru"],
+      },
+    },
+  },
+
+  plugins: [],
+};
