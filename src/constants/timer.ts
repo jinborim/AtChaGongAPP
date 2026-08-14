ca4ca13b21b2363e38d2d091cbd19109cefe1159
gmlwlsdl <heejin094@naver.com>
2026-08-14 10:54:44 +0900

    feat: 타이머 설정 정책 고정

--- src/constants/timer.ts
+++ src/constants/timer.ts
@@ -1,9 +1,10 @@
-export const MIN_FOCUS_MINUTES = 5;
-export const MAX_FOCUS_MINUTES = 120;
+export const MIN_FOCUS_MINUTES = 25;
+export const MAX_FOCUS_MINUTES = 60;
 export const FOCUS_MINUTES_STEP = 5;
 export const DEFAULT_FOCUS_MINUTES = 25;
 export const BREAK_MINUTES = 5;
+export const DEFAULT_BEVERAGE_ID = 1;
 
-export const MIN_CYCLE_COUNT = 1;
+export const MIN_CYCLE_COUNT = 4;
 export const MAX_CYCLE_COUNT = 4;
 export const CYCLE_COUNT_STEP = 1;
